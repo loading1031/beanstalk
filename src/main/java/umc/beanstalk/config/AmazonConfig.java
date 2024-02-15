11bc649bcbb329dd8e657222eb6536a83cbb4124
loading1031 <tjdans1031@gmail.com>
2024-02-15 18:16:25 +0900

    :white_check_mark: Add: bucke, reviewPath, region field

--- src/main/java/umc/beanstalk/config/AmazonConfig.java
+++ src/main/java/umc/beanstalk/config/AmazonConfig.java
@@ -16,14 +16,22 @@ public class AmazonConfig {
 
     private AWSCredentials awsCredentials;
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.path.review}")
+    private String reviewPath;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
     @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
+
 
     @PostConstruct
     public void init() {
